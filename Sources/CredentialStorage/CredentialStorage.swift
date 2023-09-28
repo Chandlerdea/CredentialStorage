4d25245153f2e9aadf227c14d375fcb60409ca3c
Chandler De Angelis <chandler.dea@me.com>
2023-09-28 16:59:08 -0700

    Add multiple credentials per protection space

--- Sources/CredentialStorage/CredentialStorage.swift
+++ Sources/CredentialStorage/CredentialStorage.swift
@@ -13,21 +13,23 @@ public struct CredentialStore {
     ///     - token: The auth token to store
     ///     - user: The user owning the auth token. This is usually the username or email belonging to the password. For auth tokens, you can use whatever description you want.
     ///     - url: The `URL` associated with the auth token
+    ///     - isDefault: Whether the token is the default credentail for the URL's protection space
     ///     - persistence: How long the token should be persisted. When this is `nil`, the default store will use `.permanent`
-    public var storeToken: (_ token: String, _ user: String, _ url: URL, _ persistence: URLCredential.Persistence?) throws -> Void
+    public var storeToken: (_ token: String, _ user: String, _ url: URL, _ isDefault: Bool, _ persistence: URLCredential.Persistence?) throws -> Void
 
     /// Removes a token for a `URL`
     /// - Parameters
     ///     - url: THe `URL` of the associated auth token
-    public var removeToken: (_ url: URL) throws -> Void
+    public var removeTokens: (_ url: URL) throws -> Void
 
     /// Retrieves an auth token for a `URL`
+    /// - Returns: A dictionary where the keys are the users and the values are the tokens
     /// - Parameters
     ///     - url: The `URL` associated with the auth token
-    public var token: (_ url: URL) throws -> String?
+    public var tokens: (_ url: URL) throws -> [String: String]
 }
 
-private extension CredentialStore {
+extension CredentialStore {
     static func protectionSpace(for host: String, url: URL) throws -> URLProtectionSpace {
         guard let scheme = url.supportedScheme, url.isSchemeSupported else {
             throw Error.unsupportedURLScheme(url.scheme)
@@ -46,34 +48,43 @@ public extension CredentialStore {
 
     /// Uses Apple's `URLCredentialStorage` to store and retreive `URLCredential` instances. This assumes the urls are using SSL (https).
     static var `default`: Self {
-        .init(
-            storeToken: { token, user, url, _ in
+        let storage = URLCredentialStorage.shared
+        return .init(
+            storeToken: { token, user, url, isDefault, persistence in
                 guard let host = url.host else {
                     throw Error.noURLHost(url)
                 }
                 let protectionSpace = try Self.protectionSpace(for: host, url: url)
-                let credential = URLCredential(user: user, password: token, persistence: .permanent)
-                URLCredentialStorage.shared.setDefaultCredential(credential, for: protectionSpace)
-                NotificationCenter.default.post(name: .NSURLCredentialStorageChanged, object: URLCredentialStorage.shared)
+                let credential = URLCredential(user: user, password: token, persistence: persistence ?? .permanent)
+                if isDefault {
+                    storage.setDefaultCredential(credential, for: protectionSpace)
+                } else {
+                    storage.set(credential, for: protectionSpace)
+                }
+                NotificationCenter.default.post(name: .NSURLCredentialStorageChanged, object: storage)
             },
-            removeToken: { url in
+            removeTokens: { url in
                 guard let host = url.host else {
                     throw Error.noURLHost(url)
                 }
                 let protectionSpace = try Self.protectionSpace(for: host, url: url)
-                guard let credential = URLCredentialStorage.shared.defaultCredential(for: protectionSpace) else {
+                guard let credentials = storage.credentials(for: protectionSpace) else {
                     return
                 }
-                URLCredentialStorage.shared.remove(credential, for: protectionSpace)
-                NotificationCenter.default.post(name: .NSURLCredentialStorageChanged, object: URLCredentialStorage.shared)
+                for (_, credential) in credentials {
+                    storage.remove(credential, for: protectionSpace)
+                }
+                NotificationCenter.default.post(name: .NSURLCredentialStorageChanged, object: storage)
             },
-            token: { url in
+            tokens: { url in
                 guard let host = url.host else {
                     throw Error.noURLHost(url)
                 }
                 let protectionSpace = try Self.protectionSpace(for: host, url: url)
-                let credential = URLCredentialStorage.shared.defaultCredential(for: protectionSpace)
-                return credential?.password
+                guard let credentials = storage.credentials(for: protectionSpace) else {
+                    return [:]
+                }
+                return credentials.compactMapValues(\.password)
             }
         )
     }
--- Tests/CredentialStorageTests/CredentialStorageTests.swift
+++ Tests/CredentialStorageTests/CredentialStorageTests.swift
@@ -7,26 +7,66 @@ final class CredentialStorageTests: XCTestCase {
     let unsupportedSchemeURL = URL(string: "facetime://www.google.com")!
     let fileURL = URL(string: "file:///usr/bin")!
 
-    func createAndAddToken(for url: URL) throws {
+    var protectionSpace: URLProtectionSpace {
+        try! CredentialStore.protectionSpace(for: supportedURL.host()!, url: supportedURL)
+    }
+
+    var storage: URLCredentialStorage {
+        URLCredentialStorage.shared
+    }
+
+    func createAndAddToken(
+        for url: URL,
+        isDefault: Bool = false
+    ) throws {
         try store.storeToken(
             "1234",
             "me@gmail.com",
             url,
+            isDefault,
             .forSession
         )
     }
 
+    override class func tearDown() {
+        super.tearDown()
+        let supportedURL = URL(string: "https://www.google.com")!
+        let protectionSpace = try! CredentialStore.protectionSpace(for: supportedURL.host()!, url: supportedURL)
+        if let allCredentials = URLCredentialStorage.shared.credentials(for: protectionSpace) {
+            for (_, credential) in allCredentials {
+                URLCredentialStorage.shared.remove(credential, for: protectionSpace)
+            }
+        }
+    }
+
+    func testStoreAddsDefaultCredential() throws {
+        try createAndAddToken(for: supportedURL, isDefault: true)
+        let credential = storage.defaultCredential(for: protectionSpace)
+        XCTAssertTrue(credential?.password == "1234")
+    }
+
     func testStoreAddsCredential() throws {
         try createAndAddToken(for: supportedURL)
-        let token = try store.token(supportedURL)
-        XCTAssertTrue(token == "1234")
+        let credentials = storage.credentials(for: protectionSpace)
+        XCTAssertEqual(credentials?.count, 1)
+        XCTAssertEqual(credentials?["me@gmail.com"]?.password, "1234")
     }
 
     func testStoreRemovesCredential() throws {
         try createAndAddToken(for: supportedURL)
-        try store.removeToken(supportedURL)
-        let token = try store.token(supportedURL)
-        XCTAssertNil(token)
+        try CredentialStore.default.removeTokens(supportedURL)
+        let credentials = storage.credentials(for: protectionSpace)
+        XCTAssertNil(credentials)
+    }
+
+    func testStoreReturnsAllTokens() throws {
+        let credential1 = URLCredential(user: "user1", password: "1", persistence: .forSession)
+        let credential2 = URLCredential(user: "user2", password: "2", persistence: .forSession)
+        storage.set(credential1, for: protectionSpace)
+        storage.set(credential2, for: protectionSpace)
+        let tokens = try CredentialStore.default.tokens(supportedURL)
+        XCTAssertEqual(tokens["user1"], "1")
+        XCTAssertEqual(tokens["user2"], "2")
     }
 
     func testUnsupportedURLCannotAddCredential() throws {
@@ -40,7 +80,7 @@ final class CredentialStorageTests: XCTestCase {
     func testUnsupportedURLCannotRemoveCredential() throws {
         try createAndAddToken(for: supportedURL)
         do {
-            try store.removeToken(unsupportedSchemeURL)
+            try store.removeTokens(unsupportedSchemeURL)
         } catch CredentialStore.Error.unsupportedURLScheme(let scheme) {
             XCTAssertEqual(scheme, "facetime")
         }
@@ -57,7 +97,7 @@ final class CredentialStorageTests: XCTestCase {
     func testFileURLCannotRemoveCredential() throws {
         try createAndAddToken(for: supportedURL)
         do {
-            try store.removeToken(fileURL)
+            try store.removeTokens(fileURL)
         } catch CredentialStore.Error.noURLHost(let url) {
             XCTAssertEqual(url, fileURL)
         }
